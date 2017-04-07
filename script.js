9b5ff746b13eeeaa65ee04bafde14f7b678afefc
Boet Waldbjorn <boetw@me.com>
2017-04-07 15:00:43 -0700

    stylzing searhc page

--- script.js
+++ script.js
@@ -10,16 +10,13 @@ $(document).ready(function() {
         });
     });
 
-    $("#yards").click(function() {
-    	console.log("click");
         $.getJSON("lists/yarders.json", function(result) {
         	console.log("got data");
             $.each(result, function(owners, field) {
                 $.each(field, function(i, passing) {
-                        $(".yarders").append("<li id='yarder"+passing.id+"'class='list-group-item'>"+passing.name + " lives at " + passing.location +" ("+passing.latLong+")"+ " and has a "+passing.yardSize+" size yard.</li>");
+                        $(".yarders").append("<li class='list-group-item'><img class='yardImage' alt='yard' src='./images/"+passing.image+".jpg'><p><strong>"+passing.location+"</strong></p></li>");
                 });
             });
         });
-    });
 
 });
